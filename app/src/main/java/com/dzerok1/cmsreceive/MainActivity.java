ea00500fc852d0db506d1ae8b6f05559b4705d22
DzeroK <dzerok2020@gmail.com>
2022-05-23 09:11:50 +0700

    core

---
+++ app/src/main/java/com/dzerok1/cmsreceive/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dzerok1.cmsreceive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
